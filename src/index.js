83b6a1a26533cd0d196e7e7fe0836c70b0934aa7
Shako Yevhen <schako15101992@gmail.com>
2022-08-05 13:44:38 +0300

    Підключив бібілітекуи

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const inputEll = document.querySelector('input');
+const listEll = document.querySelector('.country-list');
+const infoDivEll = document.querySelector('.country-info')
+const bodyEll = document.querySelector('body');
+
 
 const DEBOUNCE_DELAY = 300;
+
+
+function fetchCountries() {
+    fetch('https://restcountries.com/v2/name/united')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Data handling
+        })
+        .catch(error => {
+            // Error handling
+        }, DEBOUNCE_DELAY);
+}
+
+// fetchCountries();
+
+bodyEll.addEventListener('click', fetchCountries);
+
+// bodyEll.addEventListener(
+//     "scroll",
+//     _.debounce((fetchCountries) => {
+//         // eventCounter.debounced += 1;
+//         // debouncedOutput.textContent = eventCounter.debounced;
+//     }, DEBOUNCE_DELAY)
+// );
